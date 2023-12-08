1573f15e354c6085c6328ad0ba77ab8e6e5c31d1
Georg Boekman <georgbokman@outlook.com>
2023-12-08 18:14:52 +0100

    Update with larger release of weights

--- download_weights.sh
+++ download_weights.sh
@@ -7,10 +7,10 @@ wget -P model_weights/ https://github.com/Parskatt/DeDoDe/releases/download/dedo
 wget -P model_weights/ https://github.com/Parskatt/DeDoDe/releases/download/dedode_pretrained_models/dedode_descriptor_B.pth
 
 # Our weights
-wget -P model_weights/ https://github.com/georg-bn/rotation-steerers/releases/download/release-1/B_steerer_setting_A.pth
-wget -P model_weights/ https://github.com/georg-bn/rotation-steerers/releases/download/release-1/B_SO2_Spread_descriptor_setting_B.pth
-wget -P model_weights/ https://github.com/georg-bn/rotation-steerers/releases/download/release-1/B_SO2_Spread_steerer_setting_B.pth
-wget -P model_weights/ https://github.com/georg-bn/rotation-steerers/releases/download/release-1/B_C4_Perm_descriptor_setting_C.pth
-wget -P model_weights/ https://github.com/georg-bn/rotation-steerers/releases/download/release-1/B_C4_Perm_steerer_setting_C.pth
+wget -P model_weights/ https://github.com/georg-bn/rotation-steerers/releases/download/release-2/B_C4_steerer_setting_A.pth
+wget -P model_weights/ https://github.com/georg-bn/rotation-steerers/releases/download/release-2/B_SO2_Spread_descriptor_setting_B.pth
+wget -P model_weights/ https://github.com/georg-bn/rotation-steerers/releases/download/release-2/B_SO2_Spread_steerer_setting_B.pth
+wget -P model_weights/ https://github.com/georg-bn/rotation-steerers/releases/download/release-2/B_C4_Perm_descriptor_setting_C.pth
+wget -P model_weights/ https://github.com/georg-bn/rotation-steerers/releases/download/release-2/B_C4_Perm_steerer_setting_C.pth
 
 exit 0
